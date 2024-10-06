96c0e8d836c7b258ee3e76285bf9dfcc97543029
paiabhishekpai <abhishekupai1124@gmail.com>
2024-10-06 21:16:30 +0530

    new songs

--- musicLibrary.py
+++ musicLibrary.py
@@ -3,8 +3,8 @@ music = {
     "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
     "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
-    "kanha": "https://youtube.com/shorts/QGLIa51OfZk?si=Rqns8nwwqDDDBxlw",
-    "dynamite": "https://youtube.com/shorts/0Zjnw0S54kI?si=vkPHzfFO5n0kpWFN",
+    "kanha": "https://youtu.be/MPxDwpQJegY?si=HAITiazjT9BPpfyX",
+    "dynamite": "https://youtu.be/Mzk6KkyxXC8?si=M02nPAE00Pa9q_Zq",
     "dil":"https://youtube.com/shorts/b9cVv4RCQv0?si=Tz8CYCW4GSGFSy9h",
     "husn":"https://youtube.com/shorts/GlVT-K721ZE?si=o-8Umsi5eXwmque9"
 }
